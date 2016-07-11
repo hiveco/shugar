42dcab91aa0d362ac11df4a4d310b686273a02c7
Danny Bendelman <dbendelman@users.noreply.github.com>
2016-07-11 01:38:33 -0400

    Added docker get_file_contents methods

--- docker.sh
+++ docker.sh
@@ -20,25 +20,35 @@ docker.invoke()
 	$cmd
 }
 
-docker.container.get_id() # name
+docker.container.get_id() # container
 {
 	local id=$(docker.invoke ps -a | grep "$1\s*\$" | awk '{print $1}')
 	[[ "$id" = "" ]] && return 1
 	echo "$id"
 }
 
-docker.container.exists() # name
+docker.container.exists() # container
 {
 	docker.container.get_id "$1" >/dev/null
 }
 
-docker.container.is_running() # name
+docker.container.is_running() # container
 {
-	docker.invoke inspect -f '{{ .State.Running }}' "$1" 2>/dev/null | grep true >/dev/null
+	docker.invoke inspect -f {{.State.Running}} "$1" 2>/dev/null | grep true >/dev/null
 }
 
 docker.container.remove() # container
 {
 	docker.invoke stop "$1"
 	docker.invoke rm -v "$1"
+}
+
+docker.container.get_file_contents() # container path
+{
+	docker.invoke exec "$1" cat "$2" 2>/dev/null
+}
+
+docker.image.get_file_contents() # image path
+{
+	docker.invoke run -t --rm "$1" cat "$2" 2>/dev/null
 }
