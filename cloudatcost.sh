6de1a3aacc23e3571d710c757936435fb3008b5e
Danny Bendelman <dbendelman@users.noreply.github.com>
2016-08-10 17:36:26 -0400

    Added HTTP multi-verb support to cloudatcost API calls

--- cloudatcost.sh
+++ cloudatcost.sh
@@ -9,22 +9,22 @@ cloudatcost.auth() # login key
     cloudatcost_key="$2"
 }
 
-cloudatcost.api() # operation params
+cloudatcost.api() # http_verb operation params
 {
-    local operation="$1"
-    local params="$2"
+    local http_verb="$1"
+    local operation="$2"
+    local params="$3"
 
     if [ -z "$cloudatcost_login" ] || [ -z "$cloudatcost_key" ]; then
         echo >&2 "Error: Authentication not Configured"
         return 1
     fi
 
-    local quotes="'\""
     local url="https://panel.cloudatcost.com/api/v1/$operation.php"
     local data="key=$cloudatcost_key&login=$cloudatcost_login&$params"
-    [ $cloudatcost_verbose -eq 0 ] || echo >&2 "cloudatcost.api('$operation', '$params'): Generated URL '$url' and data '$data'"
+    [ $cloudatcost_verbose -eq 0 ] || echo >&2 "cloudatcost.api('$http_verb', '$operation', '$params'): Generated URL '$url' and data '$data'"
 
-    local response=$(curl --silent --insecure --data "$data" "$url")
+    local response=$(curl --silent --insecure -X $http_verb --data "$data" "$url")
     local status=$(echo "$response" | jq .status | sed "s/\"//g")
 
     if [ "$status" != "ok" ]; then
@@ -39,7 +39,7 @@ cloudatcost.api() # operation params
 
 cloudatcost.list_servers()
 {
-    cloudatcost.api listservers
+    cloudatcost.api GET listservers
 }
 
 cloudatcost.power_operation() # server_id poweron|poweroff|reset
@@ -47,5 +47,5 @@ cloudatcost.power_operation() # server_id poweron|poweroff|reset
     local server_id="$1"
     local operation="$2"
 
-    cloudatcost.api powerop "sid=$server_id&action=$operation"
+    cloudatcost.api POST powerop "sid=$server_id&action=$operation"
 }
