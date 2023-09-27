61f57242d830005d0b48a4deb165debc1886449b
Danny Bendelman <danny@hivecollective.co>
2023-09-27 02:05:25 -0400

    Bugfix: Retry module was using Bash syntax

--- retry.sh
+++ retry.sh
@@ -1,14 +1,15 @@
 retry() # cmd
 {
-    cmd="$@"
-
     retry_count=0
     retry_limit=4
 
-    until $cmd; do
-        (( retry_count++ >= retry_limit )) && echo 'Exceeded retry limit' && return 1
-        echo 'Waiting 5s to retry...'
-        sleep 1
+    until $@; do
+        retry_count=$(( retry_count + 1 ))
+        if [ "$retry_count" -ge "$retry_limit" ]; then
+            echo 'Exceeded retry limit'
+            return 1
+        fi
+        echo 'Waiting 3s to retry...'
+        sleep 3
     done
-
 }
